8b16bebed81b0919aeb50f1381f7fb42bb946a6b
Dwaine Lee <dlee.laba@solvd.com>
2023-04-13 19:50:31 -0500

    initial commit

---
+++ com/solvd/dwaine/lab1/UserInfo.java
@@ -0,0 +1,9 @@
+package com.solvd.dwaine.lab1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("User's Name: " + args[0]);
+        System.out.println("City: " + args[1]);
+        System.out.println("Age: " + args[2]);
+    }
+}
